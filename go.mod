414d5c261496b4342c6e3bed05c49637c53fed2d
Jonathan King <jonathankingfc@gmail.com>
2021-02-12 13:23:32 -0500

    added config creation

--- go.mod
+++ go.mod
@@ -4,5 +4,13 @@ go 1.15
 
 require (
 	github.com/containers/podman/v2 v2.2.1
+	github.com/google/go-cmp v0.5.4 // indirect
+	github.com/mattn/go-runewidth v0.0.10 // indirect
+	github.com/onsi/ginkgo v1.15.0 // indirect
+	github.com/onsi/gomega v1.10.5 // indirect
 	github.com/opencontainers/runtime-spec v1.0.3-0.20200817204227-f9c09b4ea1df
+	github.com/quay/config-tool v0.1.4
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/spf13/cobra v1.1.3 // indirect
+	google.golang.org/grpc v1.35.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path"
@@ -83,9 +84,9 @@ func setupPostgres(connText context.Context, installPath string) {
 		},
 	}
 	s.Env = map[string]string{
-		"POSTGRESQL_USER":            "quay-database",
-		"POSTGRESQL_PASSWORD":        "secret-password",
-		"POSTGRESQL_ADMIN_PASSWORD":  "secret-password",
+		"POSTGRESQL_USER":            "user",
+		"POSTGRESQL_PASSWORD":        "password",
+		"POSTGRESQL_ADMIN_PASSWORD":  "password",
 		"POSTGRESQL_DATABASE":        "quay-database",
 		"POSTGRESQL_SHARED_BUFFERS":  "256MB",
 		"POSTGRESQL_MAX_CONNECTIONS": "2000",
@@ -117,16 +118,19 @@ func setupQuay(connText context.Context, installPath string) {
 
 	log.Printf("Setting up Quay service\n")
 
-	// Set up Quay local storage
-	quayStoragePath := path.Join(installPath, "quay-storage")
-	err := os.Mkdir(quayStoragePath, 0755)
-	check(err)
-
 	// Build Quay Config
 	quayConfigPath := path.Join(installPath, "quay-config")
-	err = os.Mkdir(quayConfigPath, 0755)
+	err := os.Mkdir(quayConfigPath, 0755)
+	check(err)
+	configBytes, err := createConfigBytes()
+	check(err)
+	err = ioutil.WriteFile(path.Join(quayConfigPath, "config.yaml"), configBytes, 0644)
 	check(err)
 
+	// Set up Quay local storage
+	quayStoragePath := path.Join(installPath, "quay-storage")
+	err = os.Mkdir(quayStoragePath, 0755)
+	check(err)
 	_, err = exec.Command("setfacl", "-m", "u:1001:-wx", quayStoragePath).Output()
 	check(err)
 
@@ -166,5 +170,94 @@ func setupQuay(connText context.Context, installPath string) {
 	running := define.ContainerStateRunning
 	_, err = containers.Wait(connText, r.ID, &running)
 	check(err)
+
+}
+
+func createConfigBytes() ([]byte, error) {
+
+	// FIX THIS
+	// Create base Quay config
+	// options := generate.AioiInputOptions{
+	// 	DatabaseURI:    "postgresql://user:password@localhost:5432/quay-database",
+	// 	ServerHostname: "localhost:8080",
+	// 	RedisHostname:  "localhost",
+	// 	RedisPassword:  "strong-password",
+	// }
+	// config, err := generate.GenerateBaseConfig(options)
+	// check(err)
+
+	// configBytes, err := yaml.Marshal(config)
+	// check(err)
+	// fmt.Println(string(configBytes))
+
+	configBytes := []byte(`AUTHENTICATION_TYPE: Database
+BUILDLOGS_REDIS:
+  host: localhost
+  password: strongpassword
+  port: 6379
+DATABASE_SECRET_KEY: "81541057085600720484162638317561463611194901378275494293746615390984668417511"
+DB_URI: postgresql://user:password@localhost/quay-database
+DEFAULT_TAG_EXPIRATION: 2w
+DISTRIBUTED_STORAGE_DEFAULT_LOCATIONS: []
+DISTRIBUTED_STORAGE_PREFERENCE:
+  - localstorage
+ENTERPRISE_LOGO_URL: /static/img/quay-horizontal-color.svg
+FEATURE_ACI_CONVERSION: false
+FEATURE_ANONYMOUS_ACCESS: true
+FEATURE_APP_REGISTRY: false
+FEATURE_APP_SPECIFIC_TOKENS: true
+FEATURE_BUILD_SUPPORT: false
+FEATURE_CHANGE_TAG_EXPIRATION: true
+FEATURE_DIRECT_LOGIN: true
+FEATURE_PARTIAL_USER_AUTOCOMPLETE: true
+FEATURE_REPO_MIRROR: false
+FEATURE_MAILING: false
+MAIL_USERNAME: jonathan
+MAIL_PASSWORD: king
+MAIL_USE_AUTH: true
+FEATURE_REQUIRE_TEAM_INVITE: true
+FEATURE_RESTRICTED_V1_PUSH: true
+FEATURE_SECURITY_NOTIFICATIONS: true
+FEATURE_SECURITY_SCANNER: false
+FEATURE_USERNAME_CONFIRMATION: true
+FEATURE_USER_CREATION: true
+FEATURE_USER_LOG_ACCESS: true
+GITHUB_LOGIN_CONFIG: {}
+GITHUB_TRIGGER_CONFIG: {}
+GITLAB_TRIGGER_KIND: {}
+LOGS_MODEL: database
+LOGS_MODEL_CONFIG: {}
+LOG_ARCHIVE_LOCATION: default
+MAIL_DEFAULT_SENDER: support@quay.io
+MAIL_PORT: 587
+MAIL_USE_TLS: true
+PREFERRED_URL_SCHEME: http
+REGISTRY_TITLE: Red Hat Quay
+REGISTRY_TITLE_SHORT: Red Hat Quay
+REPO_MIRROR_SERVER_HOSTNAME: null
+REPO_MIRROR_TLS_VERIFY: true
+SECRET_KEY: "30824339799025335633887256663000123118247018465144108496567331049820667127217"
+SECURITY_SCANNER_ISSUER_NAME: security_scanner
+SERVER_HOSTNAME: quay
+SETUP_COMPLETE: true
+SUPER_USERS:
+  - user
+TAG_EXPIRATION_OPTIONS:
+  - 0s
+  - 1d
+  - 1w
+  - 2w
+  - 4w
+TEAM_RESYNC_STALE_TIME: 60m
+TESTING: false
+USERFILES_LOCATION: default
+USERFILES_PATH: userfiles/
+USER_EVENTS_REDIS:
+  host: 192.168.250.159
+  password: strongpassword
+  port: 6379
+USE_CDN: false`)
+
+	return configBytes, nil
 
 }
