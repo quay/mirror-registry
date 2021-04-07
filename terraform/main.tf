5513186f5bed7e19417ab9e244b5956f20cc814c
Jonathan King <jonathankingfc@gmail.com>
2021-04-07 13:28:23 -0400

    updated main.tf to allow ssh

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,9 +30,29 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 
+  metadata = {
+    ssh-keys = "jonathan:${file("~/.ssh/gcp.pub")}"
+  }
+
+  tags = ["jonathan-rhel-1"]
+
   network_interface {
     network = google_compute_network.vpc_network.name
     access_config {
     }
   }
+}
+
+resource "google_compute_firewall" "ssh-rule" {
+  name    = "vm-ssh"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "8080", "443", "8443"]
+  }
+  allow {
+    protocol = "icmp"
+  }
+  target_tags   = ["jonathan-rhel-1"]
+  source_ranges = ["0.0.0.0/0"]
 }
