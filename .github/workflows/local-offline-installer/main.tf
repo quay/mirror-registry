8d72bb977e785194e728c748b7f8438f29efaeb4
Dave O'Connor <doconnor@redhat.com>
2022-08-15 10:25:50 -0400

    Update Quay to 3.7.6

--- .github/workflows/local-offline-installer/main.tf
+++ .github/workflows/local-offline-installer/main.tf
@@ -26,7 +26,7 @@ resource "google_compute_network" "vpc_network_local_offline_install" {
 
 resource "google_compute_instance" "vm_instance_local_offline_install" {
   name         = "mirror-ci-rhel-local-offline-install"
-  machine_type = "e2-standard-8"
+  machine_type = "e2-standard-16"
 
   boot_disk {
     initialize_params {
